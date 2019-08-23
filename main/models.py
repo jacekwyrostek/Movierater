602d65ac21be5ccf85d8f3f3fc921582a2e8dadd
Jacek Wyrostek <jacek.wyrostek@gmail.com>
2019-08-23 13:35:58 +0200

    Add new fields in model Movie and add gitignore

--- main/models.py
+++ main/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class Movie(models.Model):
     name=models.CharField(max_length=128)
+    description=models.TextField(default='')
+    year=models.IntegerField(null=True, blank=True)
+    released=models.DateField(null=True, blank=True)
+    imdRating=models.DecimalField(null=True, blank=True, decimal_places=7, max_digits=10)
+    photo=models.ImageField(null=True, blank=True)

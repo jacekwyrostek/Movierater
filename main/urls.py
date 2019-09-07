1267c29b0b56f29efc81b4ac8f96a038e1a4dcaf
Jacek Wyrostek <jacek.wyrostek@gmail.com>
2019-09-07 18:06:23 +0200

    Add 'edit' subpage

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import movieList, addMovie
+from .views import movieList, addMovie, editMovie
 
 urlpatterns = [
     path('list/', movieList),
-    path('add/', addMovie)
+    path('add/', addMovie),
+    path('edit/<int:id>', editMovie),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Movie
 from .forms import MovieForm
 
@@ -9,8 +9,15 @@ def movieList(request):
     return render(request, 'movie_list.html',{'movies':movies})
 
 def addMovie(request):
-    form=MovieForm(request.POST or None, request.FILES or None)
+    form=MovieForm(request.POST or None, request.FILES or None,)
     if form.is_valid():
         form.save()
         return redirect(movieList)
-    return render(request, 'new_movie.html', {'form':form})
+    return render(request, 'movie_form.html', {'form':form})
+def editMovie(request, id):
+    movie=get_object_or_404(Movie, pk=id)
+    form=MovieForm(request.POST or None, request.FILES or None, instance=movie)
+    if form.is_valid():
+        form.save()
+        return redirect(movieList)
+    return render(request, 'movie_form.html', {'form':form})

2acf5df955eb4dcad6b8241a205d0c93870ef089
Jacek Wyrostek <jacek.wyrostek@gmail.com>
2019-09-11 19:50:07 +0200

    changes in links syntax

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from .views import movieList, addMovie, editMovie, deleteMovie
 
 urlpatterns = [
-    path('list/', movieList),
-    path('add/', addMovie),
-    path('edit/<int:id>', editMovie),
-    path('delete/<int:id>', deleteMovie),
+    path('list/', movieList, name='movieList'),
+    path('add/', addMovie, name='addMovie'),
+    path('edit/<int:id>', editMovie, name='editMovie'),
+    path('delete/<int:id>', deleteMovie, name='deleteMovie'),
 ]
